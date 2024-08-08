7aa1a03beb7babbb13bc261a101c49e299991899
SuleWare <suleware@SuleWare.org>
2024-08-08 20:58:29 +0200

    first tests

--- node-ts-bases-seccion-4/src/app.ts
+++ node-ts-bases-seccion-4/src/app.ts
@@ -2,35 +2,26 @@ import { getPokemonById } from "./js-foundation/06-promises";
 import { buildLogger } from "./plugins/logger.plugin";
 // const { getAge, getUUID } = require('./plugins');
 
-
 // const { emailTemplate } = require('./js-foundation/01-template');
 // require('./js-foundation/02-destructuring');
 // const { getUserById } = require('./js-foundation/03-callbacks');
 // const { getUserById } = require('./js-foundation/04-arrow');
 // const { buildMakePerson } = require('./js-foundation/05-factory')
 
-
 // const getPokemonById = require('./js-foundation/06-promises');
 
- 
-const logger = buildLogger('app.js');
+// const logger = buildLogger('app.js');
 
-logger.log('Hola mundo');
-logger.error('Esto es algo malo');
+// logger.log('Hola mundo');
+// logger.error('Esto es algo malo');
 
 // getPokemonById(4)
 //   .then( ( pokemon ) => console.log({ pokemon }) )
 //   .catch( ( err ) => console.error( err ) )
 //   .finally( () => console.log('Finalmente') );
 
-
-
-
-
-
 // token de acceso
 // Publicas
-
 
 // ! Referencia a la función factory y uso
 // const makePerson = buildMakePerson({ getUUID, getAge });
@@ -40,9 +31,3 @@ logger.error('Esto es algo malo');
 // const john = makePerson( obj );
 
 // console.log({ john });
-
-
-
-
-
-
