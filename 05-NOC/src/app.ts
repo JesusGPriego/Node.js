b0fa9e4c0bb3dce54f10804def376edc741e5532
JesusGPriego <suleware@gmail.com>
2024-09-01 20:20:40 +0200

    postgre integration

--- 05-NOC/src/app.ts
+++ 05-NOC/src/app.ts
@@ -1,23 +1,22 @@
 import { Server } from '@/presentation/server';
 import { envs } from '@/config/env';
 import { MongoDatabase } from '@/data/mongo/init';
-import { LogModel } from '@/data/mongo/models/log.model';
+import { PrismaClient } from '@prisma/client';
 
 (() => {
   main();
 })();
 
 async function main() {
-  await MongoDatabase.connect({
-    mongoUrl: envs.MONGO_URL,
-    dbName: envs.MONGO_DB_NAME,
-  });
+  // await MongoDatabase.connect({
+  //   mongoUrl: envs.MONGO_URL,
+  //   dbName: envs.MONGO_DB_NAME,
+  // });
   Server.start();
 }
 
 async function main2() {
-  await MongoDatabase.connect({
-    mongoUrl: envs.MONGO_URL,
-    dbName: envs.MONGO_DB_NAME,
-  });
+  // const prisma = new PrismaClient();
+  // const logs = await prisma.logModel.findMany();
+  // console.log(logs);
 }
--- 05-NOC/src/presentation/server.ts
+++ 05-NOC/src/presentation/server.ts
@@ -1,14 +1,16 @@
 import { envs } from '@/config/env';
 import { EmailPlugin } from '@/config/mailer/mailer.plugin';
+import { LogSeverityLevel } from '@/domain/entities/log.entity';
 import { CheckService } from '@/domain/use-cases/checks/check-service';
 import { EmailService } from '@/domain/use-cases/email/email-service';
 import { FileSystemDataSource } from '@/infra/datasources/file-system.datasource';
 import { MongoLogDatasource } from '@/infra/datasources/mongo-log.datasource';
+import { PostgresLogDatasource } from '@/infra/datasources/postgres-log.datasource';
 import { LogRepositoryImpl } from '@/infra/repositories/log-impl.repository';
 import { CronService } from '@/presentation/cron/cron.service';
 const defaultURL = 'https://www.google.com/';
 
-const logRepository = new LogRepositoryImpl(new MongoLogDatasource());
+const logRepository = new LogRepositoryImpl(new PostgresLogDatasource());
 
 const emailPlugin = new EmailPlugin({
   service: envs.MAILER_SERVICE,
@@ -34,15 +36,12 @@ function checkService(url: string = defaultURL): void {
   ).execute(url);
 }
 
-async function getLogs() {
-  const logs = await logRepository.getLogs();
-  console.log(logs);
-}
-
 export class Server {
-  public static start() {
+  public static async start() {
     console.log('Server started');
-    getLogs();
+    // const logs = await logRepository.getLogsBySeverityLevel(
+    //   LogSeverityLevel.low
+    // );
     // CronService.createJob(
     //   CronService.createCronTimeSeconds(5),
     //   onTick(checkService)
