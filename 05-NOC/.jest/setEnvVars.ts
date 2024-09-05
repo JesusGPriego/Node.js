1c8d2aec6b900bd6b310665937d36b682f5a81ed
JesusGPriego <suleware@gmail.com>
2024-09-05 11:14:52 +0200

    test init config - native .env support for testing

---
+++ 05-NOC/.jest/setEnvVars.ts
@@ -0,0 +1,17 @@
+process.env.PORT = '3000';
+
+process.env.MAILER_SERVICE = 'gmail';
+process.env.MAILER_EMAIL = 'email@email.com';
+process.env.MAILER_SECRET_KEY = 'trythis';
+process.env.RECEIVER_EMAIL = 'email2@gmail.com';
+process.env.PROD = 'false';
+process.env.MONGO_URL = 'mongodb://suleware:01315162@localhost:27017';
+process.env.MONGO_DB_NAME = 'NOC-TEST';
+process.env.MONGO_USER = 'tester';
+process.env.MONGO_PASS = '01315162';
+
+process.env.POSTGRES_URL =
+  'postgresql://postgres:01315162@localhost:5432/NOC-TEST';
+process.env.POSTGRES_USER = 'tester';
+process.env.POSTGRES_PASSWORD = '01315162';
+process.env.POSTGRES_DB_NAME = 'NOC-TEST';
---
+++ 05-NOC/tests/config/env/index.test.ts
@@ -0,0 +1,7 @@
+import { envs } from "@/config/env"
+
+describe('envs', () => {
+    test('should return env options', () => {
+        console.log('envs', envs)
+    })
+})
